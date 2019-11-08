01551c96c470c96e4a302d756478ca27a45f254a
flexsant <flexsant@gmail.com>
2019-11-07 19:02:15 -0800

    functions for bad and good guesses

--- react-clicky-game/src/App.js
+++ react-clicky-game/src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
     topScore: 0,
     result: "Click an Image to Begin!"
   };
+
   //Function to shuffle data array
   shuffle = data => {
     let i = data.length - 1;
@@ -22,17 +23,54 @@ class App extends Component {
     }
     return data;
   };
+
   //function to reset the data change all the clicked values to false
   reset = data => {
     const resetData = data.map(item => ({ ...item, clicked: false }));
     return this.shuffle(resetData);
     //return resetData;
   };
-  //function to handle image click
 
+  //function to handle image click
+  handleImageClick = id => {
+    let goodGuess = false;
+    const newData = this.state.data.map(item => {
+      const currentItem = { ...item };
+      if (id === currentItem.id) {
+        if (!currentItem.clicked) {
+          currentItem.clicked = true;
+          goodGuess = true;
+        }
+      }
+      return currentItem;
+    });
+    if (goodGuess) {
+      this.handleGoodGuess(newData);
+    } else {
+      this.handleBadGuess(newData);
+    }
+  };
   //function to handle a good guess
+  handleGoodGuess = data => {
+    const newScore = this.state.score + 1;
+    //const newTopScore = Math.max(this.state.topScore, newScore);
+    this.setState({
+      data: this.shuffle(data),
+      score: newScore,
+      result: "You Guessed Correctly",
+      topScore: Math.max(this.state.topScore, newScore)
+    });
+  };
 
   //function to handle a bad guess
+  handleBadGuess = data => {
+    this.setState({
+      data: this.reset(data),
+      score: 0,
+      result: "Wrong! Click to try again!"
+    });
+  };
+
   render() {
     return (
       <div>
