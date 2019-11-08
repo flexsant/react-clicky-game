8e8f27f9d00e9ad05b991aa8517c0578123b37d4
flexsant <flexsant@gmail.com>
2019-11-07 18:49:38 -0800

    app.js class app extends component

--- react-clicky-game/src/App.js
+++ react-clicky-game/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import data from "./data.json";
+import Navbar from "./components/Navbar";
+import ClickImage from "./components/ClickImage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  //state
+ 
+  //Function to shuffle data array
+  //function to reset the data change all the clicked values to false
+ 
+  //function to handle image click
+  
+  //function to handle a good guess
+ 
+  //function to handle a bad guess
+  render() {
+    return (
+      <div>
+        <Navbar
+          result={this.state.result}
+          score={this.state.score}
+          topScore={this.state.topScore}
+        />
+        <div className="container">
+          <div className="row">
+            <h2 className="mx-auto">
+              Click on an image to earn points, but don't click on any more than
+              once.
+            </h2>
+          </div>
+          <div className="row">
+            {this.state.data.map(item => (
+              <ClickImage
+                handleImageClick={this.handleImageClick}
+                key={item.id}
+                {...item}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
